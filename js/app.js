c81306ed2207c14567809f7d8fbd0d7e90cab6cb
BenBlack <benjyblack@gmail.com>
2014-02-21 11:52:18 -0500

    add travel page, blog page, change images and layout

--- js/app.js
+++ js/app.js
@@ -6,13 +6,29 @@ personalApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
             when('/home', {
+            title: 'Home',
             templateUrl: 'partials/home.html'
         }).
             when('/projects', {
+            title: 'Projects',
             templateUrl: 'partials/projects.html'
+        }).
+            when('/travel', {
+            title: 'Travel',
+            templateUrl: 'partials/travel.html'
+        }).
+            when('/blog', {
+            title: 'Blog',
+            templateUrl: 'partials/blog.html'
         }).
         otherwise({
             redirectTo: '/home'
         });
   }
 ]);
+
+personalApp.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
