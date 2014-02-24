d7f03fe87b18ed0f4c4c86bfc1d43ce00a73c5cd
BenBlack <benjyblack@gmail.com>
2014-02-24 11:13:02 -0500

    update travel section with writeup, change backgound color, change map type, add location to photo captions

--- js/services.js
+++ js/services.js
@@ -46,7 +46,8 @@ personalAppServices.factory('GoogleMaps', function() {
 			var map     = new google.maps.Map($(map_id)[0], {
 				zoom : zoom,
 			    center : new google.maps.LatLng(lat, lng),
-			    mapTypeId : google.maps.MapTypeId.ROADMAP,
+			    mapTypeControl: false,
+			    mapTypeId : google.maps.MapTypeId.HYBRID,
 			    styles: [{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]}]
 			});
 
